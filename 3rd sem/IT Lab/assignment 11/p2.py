64b4a96540f139bd159ea45ac11ac2278474121c
Mayukh13 <roy.mayukh@outlook.com>
2021-01-14 00:02:24 +0530

    13-jan-2021 2

--- 3rd sem/IT Lab/assignment 11/p2.py
+++ 3rd sem/IT Lab/assignment 11/p2.py
@@ -1,6 +1,6 @@
 '''2) Write a program which will print the month name and first Monday in a given year.'''
 import calendar
-n=2021
+n=int(input('Enter year: '))
 c=calendar.Calendar(calendar.MONDAY)
 for i in range(12):
     d=c.monthdayscalendar(n,i+1)
