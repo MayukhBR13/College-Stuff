ac097ac0487f254a4b4dd8108bfbba9595547eda
Mayukh13 <roy.mayukh@outlook.com>
2021-01-14 16:41:17 +0530

    done

--- 3rd sem/IT Lab/assignment 12/p6/test.py
+++ 3rd sem/IT Lab/assignment 12/p6/test.py
@@ -12,7 +12,7 @@ F(5,6)=F(n-1,r)+F(n-1,r-1)   {mt.fun2(5,6)}
 F(5)=F(n/2)+1 if n>1         {mt.fun3(5)}
 F(5,6)=1 if M=0, or M ≥N ≥1, and F(M,N)=F(M-1,N)+F(M-1,N-1), otherwise.   {mt.fun4(5,6)}
 B(5,6)=m!/(x!(m-x)!) where m>x,       
-B(0,0)=B(m,0)=1 and B(m,x)=B(m,x-1)*[(m-x+1)/x]      {mt.fun5(5,6)}  '''
+B(0,0)=B(m,0)=1 and B(m,x)=B(m,x-1)*[(m-x+1)/x]      {mt.fun5(10,4)}  '''
 
 
 print(s)
