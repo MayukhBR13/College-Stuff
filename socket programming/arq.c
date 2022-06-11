03619765caa806acd009f5df97a8a1737f6da794
Mayukh Barman Ray <56465452+MayukhBR13@users.noreply.github.com>
2022-06-11 19:57:14 +0530

    ack added

--- socket programming/arq.c
+++ socket programming/arq.c
@@ -2,33 +2,38 @@
 #include<stdlib.h>
 #include<string.h>
 int main(){
-	int i, n, ch, ack;
+	int i, n, ch, ack,a=0,one=0;
 	char input[20];
 	FILE *in;
 	printf("\n\n\nStop and Wait Protocol\n");
 	printf("\n1.Send\n2.Check ACK\n3.EOT\n");
 	while(1){
-		printf("Enter the choice:");
+		printf("\nEnter the choice:");
 		scanf("%d",&ch);
 		switch(ch){
 			case 1:
-				ack = 0;
 				in = fopen("data.txt","w");
 				printf("Enter the data:");
 				scanf("%s",input);
 				n = strlen(input);
-				for(i = 0; i < n+1; i++){
-					fprintf(in,"%s",input);
+				for(i = 0,one = 0; i < n; i++){
+					fprintf(in,"%c",input[i]);
+					if(input[i]=='1')one++;
 				}
+				fprintf(in,"%c",(one%2==0)?'0':'1');
 				fclose(in);
 				printf("--->Data sent\n");
-				ack = 1;
+				a++;
 				break;
 			case 2:
-				if ( ack == 1 ){
+				in = fopen("ack.txt","r");
+				fscanf(in,"%d",&ack);
+				fclose(in);
+				if ( ack == a%2 ){
 					printf("Acknowledgement received.");
 				}else{
 					printf("Acknowledgement not received.");
+					a--;
 				}
                 break;
 			case 3:
